01d801e5e1d5ac59a2948d77515d22e7999179f9
amiraflak <mehrdad.aflakparast@gmail.com>
2023-11-11 14:04:19 +0330

    add: second question materials

---
+++ Q2/include/count_vowels.h
@@ -0,0 +1,6 @@
+#ifndef COUNT_VOWELS_H
+#define COUNT_VOWELS_H
+
+int count_vowels(char *sentence);
+
+#endif /* COUNT_VOWELS_H */
---
+++ Q2/src/count_vowels.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int count_vowels(char *sentence) {
+  int vowel_count = 0;
+    /*
+    TODO: Write the implementation here!
+
+    HINT: Iterate over the characters in the sentence,
+    until the end of the sentence is reached.
+
+    Input:
+        I love programming!
+    Output:
+        6
+    */
+  return vowel_count;
+}
+
+
+
+
+
+
+
+
+
+
